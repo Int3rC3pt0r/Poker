641c4d67cd650cbf041def0936b218deaa88c30e
thomasgassler <gassler0201@gmail.com>
2020-03-04 08:46:58 +0100

    added Percentages and fixed Straight-test

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,23 @@ import java.util.ArrayList;
 
 public class Main {
 
+	private static double pHigh = 50.12;
+	private static double pPair = 42.26;
+	private static double pTwoPair = 4.75;
+	private static double pThree = 2.11;
+	private static double pStraight = 0.392;
+	private static  double pFlush = 0.197;
+	private static double pFullHouse = 0.144;
+	private static double pFour = 0.024;
+	private static double pStraightFlush = 0.00139;
+	private static double pRoyalFlush = 0.000154;
+	
+	
 	private static int color = 4;
 	private static int worth = 13;
 	private static int size = color * worth;
 	private static int handSize = 5;
-	private static int repeat = 1000000;
+	private static int repeat = 10000000;
 	
 	static ArrayList<Integer> cardPack = new ArrayList<Integer>();
 	
@@ -28,7 +40,7 @@ public class Main {
 	public static ArrayList<Integer> rdmCards() {
 		ArrayList<Integer> hand = new ArrayList<Integer>();
 		for(int i=0; i < handSize; i++) {
-			int rdm = (int) (Math.random() * (size-1-i));
+			int rdm = (int) (Math.random() * (size-i));
 			hand.add(cardPack.get(rdm));
 			cardPack.add(cardPack.get(rdm));
 			cardPack.remove(rdm);
@@ -99,8 +111,13 @@ public class Main {
 		boolean sameColor = true;
 		list = insertionSortWorth(list);
 		for(int i = 0; i < list.size()-1; i++) {
-			if(list.get(i)%worth != list.get(i+1)%worth-1) {
+			if(i == list.size()-2 && list.get(i)%worth == handSize - 1 && list.get(i+1)%worth == worth - 1) {
+				street = true;
+				break;
+			}
+			else if(list.get(i)%worth != list.get(i+1)%worth-1) {
 				street = false;
+				break;
 			}
 		}
 		for(int i = 0; i < list.size()-1; i++) {
@@ -109,7 +126,7 @@ public class Main {
 			}
 		}
 		if(street) {
-			if(sameColor && list.get(list.size()-1)%13 == 12) {
+			if(sameColor && list.get(list.size()-1)%worth == worth-1 && list.get(list.size()-2)%worth == worth-2) {
 				return 4;
 			}
 			else if(sameColor){
@@ -188,17 +205,29 @@ public class Main {
 				highCard++;
 			}
 		}
-		System.out.println("Pokertrys: " + repeat);
-		System.out.println("High Card:\t" + highCard + "\t" + ((double) highCard)/repeat * 100 + "%");
-		System.out.println("Pair:\t\t" + onePair + "\t" + ((double) onePair)/repeat * 100 + "%");
-		System.out.println("Two Pairs:\t" + twoPair + "\t" + ((double) twoPair)/repeat * 100 + "%");
-		System.out.println("Three:\t\t" + three + "\t" + ((double) three)/repeat * 100 + "%");
-		System.out.println("Straight:\t" + straight + "\t" + ((double) straight)/repeat * 100 + "%");
-		System.out.println("Flush:\t\t" + flush + "\t" + ((double) flush)/repeat * 100 + "%");
-		System.out.println("Full House:\t" + house + "\t" + ((double) house)/repeat * 100 + "%");
-		System.out.println("Four:\t\t" + four + "\t" + ((double) four)/repeat * 100 + "%");
-		System.out.println("Straight Flush:\t" + straightFlush + "\t" + ((double) straightFlush)/repeat * 100 + "%");
-		System.out.println("Royal Flush:\t" + royalFlush + "\t" + ((double) royalFlush)/repeat * 100 + "%");
+//		System.out.println("Pokertrys: " + repeat);
+//		System.out.println("High Card:\t" + highCard + "	\t" + ((double) highCard)/repeat * 100 + "%\t" + pHigh + "%\t" + (((double) highCard)/repeat * 100 - pHigh));
+//		System.out.println("Pair:\t\t" + onePair + "	\t" + ((double) onePair)/repeat * 100 + "%\t" + pPair + "%\t" + (((double) onePair)/repeat * 100 - pPair));
+//		System.out.println("Two Pairs:\t" + twoPair + "	\t" + ((double) twoPair)/repeat * 100 + "%\t" + pTwoPair + "%\t" + (((double) twoPair)/repeat * 100 - pTwoPair));
+//		System.out.println("Three:\t\t" + three + "	\t" + ((double) three)/repeat * 100 + "%\t" + pThree + "%\t" + (((double) three)/repeat * 100 - pThree));
+//		System.out.println("Straight:\t" + straight + "	\t" + ((double) straight)/repeat * 100 + "%\t" + pStraight + "%\t" + (((double) straight)/repeat * 100 - pStraight));
+//		System.out.println("Flush:\t\t" + flush + "	\t" + ((double) flush)/repeat * 100 + "%\t" + pFlush + "%\t" + (((double) flush)/repeat * 100 - pFlush));
+//		System.out.println("Full House:\t" + house + "	\t" + ((double) house)/repeat * 100 + "%\t" + pFullHouse + "%\t" + (((double) house)/repeat * 100 - pFullHouse));
+//		System.out.println("Four:\t\t" + four + "	\t" + ((double) four)/repeat * 100 + "%\t" + pFour + "%\t" + (((double) four)/repeat * 100 - pFour));
+//		System.out.println("Straight Flush:\t" + straightFlush + "	\t" + ((double) straightFlush)/repeat * 100 + "%\t" + pStraightFlush + "%\t" + (((double) straightFlush)/repeat * 100 - pStraightFlush));
+//		System.out.println("Royal Flush:\t" + royalFlush + "	\t" + ((double) royalFlush)/repeat * 100 + "%\t" + pRoyalFlush + "%\t" + (((double) royalFlush)/repeat * 100 - pRoyalFlush));
+		
+		System.out.printf("Pokertrys: %d\n", repeat);
+		System.out.printf("High Card:\t%d\t%.4f\t%.3f\t\t%.6f\t%.4f\n",highCard, ((double) highCard)/repeat * 100, pHigh,(((double) highCard)/repeat * 100) - pHigh,(((double) highCard)/repeat * 100)/(pHigh/100)-100);
+		System.out.printf("Pair:\t\t%d\t%.4f\t%.3f\t\t%.6f\t%.4f\n",onePair, ((double) onePair)/repeat * 100, pPair,(((double) onePair)/repeat * 100) - pPair,(((double) onePair)/repeat * 100)/(pPair/100)-100);
+		System.out.printf("Two Pairs:\t%d\t%.4f\t%.4f\t\t%.6f\t%.4f\n",twoPair, ((double) twoPair)/repeat * 100, pTwoPair,(((double) twoPair)/repeat * 100) - pTwoPair,(((double) twoPair)/repeat * 100)/(pTwoPair/100)-100);
+		System.out.printf("Three:\t\t%d\t%.4f\t%.4f\t\t%.6f\t%.4f\n",three, ((double) three)/repeat * 100, pThree,(((double) three)/repeat * 100) - pThree,(((double) three)/repeat * 100)/(pThree/100)-100);
+		System.out.printf("Straight:\t%d\t%.4f\t%.4f\t\t%.6f\t%.4f\n",straight, ((double) straight)/repeat * 100, pStraight,(((double) straight)/repeat * 100) - pStraight,(((double) straight)/repeat * 100)/(pStraight/100)-100);
+		System.out.printf("Flush:\t\t%d\t%.4f\t%.4f\t\t%.6f\t%.4f\n",flush, ((double) flush)/repeat * 100, pFlush,(((double) flush)/repeat * 100) - pFlush,(((double) flush)/repeat * 100)/(pFlush/100)-100);
+		System.out.printf("Full House:\t%d\t%.4f\t%.4f\t\t%.6f\t%.4f\n",house, ((double) house)/repeat * 100, pFullHouse,(((double) house)/repeat * 100) - pFullHouse,(((double) house)/repeat * 100)/(pFullHouse/100)-100);
+		System.out.printf("Four:\t\t%d\t%.4f\t%.4f\t\t%.6f\t%.4f\n",four, ((double) four)/repeat * 100, pFour,(((double) four)/repeat * 100) - pFour,(((double) four)/repeat * 100)/(pFour/100)-100);
+		System.out.printf("Straight Flush:\t%d\t%.4f\t%.5f\t\t%.6f\t%.4f\n",straightFlush, ((double) straightFlush)/repeat * 100, pStraightFlush,(((double) straightFlush)/repeat * 100) - pStraightFlush,(((double) straightFlush)/repeat * 100)/(pStraightFlush/100)-100);
+		System.out.printf("Royal Flush:\t%d\t%.4f\t%.6f\t%.6f\t%.4f\n",royalFlush, ((double) royalFlush)/repeat * 100, pRoyalFlush,(((double) royalFlush)/repeat * 100) - pRoyalFlush,(((double) royalFlush)/repeat * 100)/(pRoyalFlush/100)-100);
 		
 	}
 
